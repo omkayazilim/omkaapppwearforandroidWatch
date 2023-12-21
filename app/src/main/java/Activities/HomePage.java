6c8dc28c5f51b5bbdeaf0a5efcfa048eaa0eb634
Orhan Güneş <orhangune@gmail.com>
2023-12-21 15:37:56 +0300

    layout eklendi

---
+++ app/src/main/java/Activities/HomePage.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.omkahomeassistant.R;
+
+public class HomePage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
